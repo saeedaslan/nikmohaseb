f3427d89ef13012f8241774bce60ca91dcf74e6c
saeed aslani <saeed_aslani@vasl.ir>
2026-08-14 14:39:15 +0330

    fix phone number

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -99,8 +99,8 @@ export function Footer() {
               </li>
               <li className="flex items-center gap-2.5">
                 <PhoneCall className="h-4 w-4 text-accent-green" />
-                <a href="tel:+982100000000" className="hover:text-accent-green">
-                  ۰۲۱-۱۲۳۴۵۶۷۸
+                <a href="tel:+9821828701113" className="hover:text-accent-green">
+                  ۰۲۱-۸۲۸۷۰۱۱۱۳
                 </a>
               </li>
             </ul>
